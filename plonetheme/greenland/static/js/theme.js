66569a31fd41e72fea4215704bd4ebade139f1fe
Tristan Le Toullec <tristan.letoullec@univ-brest.fr>
2012-03-26 12:03:41 +0200

    Initial version without Bibliography support

---
+++ plonetheme/greenland/static/js/theme.js
@@ -0,0 +1,5 @@
+
+function initializeplonethemegreenland(){
+
+}
+jQuery(initializeplonethemegreenland);
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='plonetheme.greenland',
+      version=version,
+      description="Blue theme for Plone and Diazo",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
