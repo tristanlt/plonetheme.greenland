efeb69895c6996187e59224abbbb3cb8f01c556a
Tristan Le Toullec <tristan.letoullec@gmail.com>
2012-03-27 16:54:06 +0200

    Adding relflecto and biblio

--- plonetheme/greenland/static/js/theme.js
+++ plonetheme/greenland/static/js/theme.js
@@ -1,5 +1,9 @@
 
 function initializeplonethemegreenland(){
-
 }
 jQuery(initializeplonethemegreenland);
+
+$(document).ready(function() {
+    $('.template-reflector_view .listing').dataTable();
+} );
+
